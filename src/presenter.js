5d4c017dfac3af39e30beec28be027e110c574b8
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-02-13 12:02:58 -0400

    New multiplication function

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,23 @@
+import multiplicar from "./multiplicador";
 import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const formCalcu = document.querySelector("#calculadora-form");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
+formCalcu.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  if(event.submitter.id === "sumar-button")
+  {
+    div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  }
+  if(event.submitter.id === "multiplicar-button")
+  {
+    div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+  }  
 });
